8822bbd06f00402da493c8d118229236d9850392
Samuel Reams <34895348+lmaoyeet@users.noreply.github.com>
2019-04-12 18:23:24 -0500

    made code easier to understand

---
+++ js/css.js
@@ -0,0 +1,21 @@
+// to show the canvas
+function show() {
+    canvas.style.display = "inline";
+    // eval(element + ".style.display='inline'");
+}
+
+// to hide the canvas
+function hide() {
+    canvas.style.display = "none";
+    // eval(element + ".style.display='none'");
+}
+
+// to hide the button
+function hideMenu() {
+    document.getElementById("start").style.display = 'none';
+}
+
+// to show the button
+function showMenu() {
+    document.getElementById("start").style.display = 'inline';
+}
--- js/sketch.js
+++ js/sketch.js
@@ -102,28 +102,6 @@ function keyPush(evt) {
     }
 }
 
-// to show the canvas
-function show() {
-    canvas.style.display = "inline";
-    // eval(element + ".style.display='inline'");
-}
-
-// to hide the canvas
-function hide() {
-    canvas.style.display = "none";
-    // eval(element + ".style.display='none'");
-}
-
-// to hide the button
-function hideMenu() {
-    document.getElementById("start").style.display = 'none';
-}
-
-// to show the button
-function showMenu() {
-    document.getElementById("start").style.display = 'inline';
-}
-
 // handling wall collision
 function walls() {
     if (snek.x < 0) {
